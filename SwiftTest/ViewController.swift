a192ce483fae78db465d0ff1141202270c775f7e
廉金涛 <lianjintao@bytedance.com>
2022-04-17 17:58:12 +0800

    测试

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by bytedance on 2022/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
